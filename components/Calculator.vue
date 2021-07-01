d082918bb3691f380652b52b01ffa26284a9d780
José Fco. Rives Lirola <jose@digio.es>
2021-07-01 11:10:33 +0200

    working

---
+++ components/Calculator.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="row align-items-center">
+        <div class="col">
+          <b-form-textarea
+            id="originalcups"
+            v-model="originalcups"
+            placeholder="Copia y pega los cups sin la letra de validación (1 por línea)"
+            rows="8"
+          ></b-form-textarea>
+        </div>
+        <div class="col">
+          <b-form-textarea
+            id="convertedcups"
+            v-model="convertedcups"
+            placeholder="cups con la letra de validación"
+            rows="8"
+          ></b-form-textarea>
+        </div>
+      </div>
+      <b-button @click="convertcups()">Convertir</b-button>
+    </div>
+</template>
+
+<script>
+
+function calculatecups(cups, validatedcups) {
+  const cupspattern = /(?:[A-Z]{2})?(\d+)(?:[A-Z]{2})?/gm;
+  const validated = cupspattern.exec(cups);
+
+  if (!validated || validated.length < 1 || validated[1].length != 16){
+    return cups + " not valid";
+  }
+
+  const cupsnumber = validated[1];
+
+  const firstmodule = parseInt(cupsnumber) % 529;
+  console.log(firstmodule);
+  const finalmodule = Math.floor(firstmodule / 23);
+  const finalrest = firstmodule % 23;
+
+  const cupsletters = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+
+  return cupsnumber + cupsletters[finalmodule] + cupsletters[finalrest];
+}
+
+export default {
+  data() {
+    return {
+      originalcups: 'ES0021000013098661',
+      convertedcups: ''
+    }
+  },
+  methods: {
+    convertcups(){
+      this.convertedcups = '';
+      const value = 'ES0021000013098661'; //MC
+
+      const allcups = this.originalcups.split('\n');
+      allcups.forEach(cups => {
+        this.convertedcups += calculatecups(cups) + "\n";
+      });
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1>Calculadora de CUPS</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
